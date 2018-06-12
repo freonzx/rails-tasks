913a05243f4debc82266ca987b44f7524d338d9f
Freon <freonzx@gmail.com>
2018-06-12 19:07:57 +0000

    Nothing

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,16 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+$(function(){
+  $(document).on("change", "input", function() {
+    $.ajax({
+    url: `/tasks/${this.value}/`,
+    type: 'PUT',
+    dataType: 'script',
+    data: `task[done]=${this.checked}`,
+    success: function(data) {
+      //alert('Load was performed.');
+    }
+    })
+  })
+});
